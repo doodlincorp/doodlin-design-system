6ac2cfc60a5c750d0870a8e3495b9534224a0fc7
dhoonjang <dhoonjang@gmail.com>
2020-12-11 18:35:00 +0900

    change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import copy from "rollup-plugin-copy-assets";
 
 const packageJson = require("./package.json");
 
@@ -26,5 +27,8 @@ export default {
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     postcss(),
+    copy({
+      assets: [packageJson.scss],
+    }),
   ],
 };
