302d95acd6f78fe27b3a7216daea06c65ff273b1
chamdasu <68911837+chamdasu@users.noreply.github.com>
2020-12-11 17:55:09 +0900

    feat: index scss

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,21 @@
-import React, { ButtonHTMLAttributes } from "react";
-import "./Button.scss";
+import React, { ButtonHTMLAttributes } from 'react';
+import './Button.scss';
 
-export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'min' | 'big';
+}
 
-const Button: React.FC<IButtonProps> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+const Button: React.FC<IButtonProps> = ({
+  size,
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <button className={`_BUTTON_ ${className} ${size}`} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
