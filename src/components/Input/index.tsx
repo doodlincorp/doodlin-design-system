d5cd8259779b57863628d7990c405517b18d99db
dhoonjang <dhoonjang@gmail.com>
2020-12-14 16:10:18 +0900

    set: input onchange

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,26 +2,10 @@ import React, { InputHTMLAttributes } from "react";
 import cn from "classnames";
 import "./Input.scss";
 
-export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  changeValue: (value: string | boolean | number) => void;
-  checkEvent?: (e: React.ChangeEvent<HTMLInputElement>) => boolean;
-}
+export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input: React.FC<IInputProps> = ({
-  changeValue,
-  checkEvent,
-  className,
-  ...props
-}) => {
-  return (
-    <input
-      className={cn("_INPUT_", className)}
-      onChange={(e) => {
-        if (!checkEvent || checkEvent(e)) changeValue(e.currentTarget.value);
-      }}
-      {...props}
-    />
-  );
+export const Input: React.FC<IInputProps> = ({ className, ...props }) => {
+  return <input className={cn("_INPUT_", className)} {...props} />;
 };
 
 export default Input;
