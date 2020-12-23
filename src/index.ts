22624fdce56d474f083ed98bbfb2f95156aba036
chamdasu <68911837+chamdasu@users.noreply.github.com>
2020-12-23 13:47:28 +0900

    set export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import './reset.scss';
-export { default as Input } from './components/Input';
 export { default as Button } from './components/Button';
+export { default as Input } from './components/Input';
+export { default as Spinner } from './components/Spinner';
+export { default as Text } from './components/Text';
+export { default as Textarea } from './components/Textarea';
 
 export type TDefaultSize =
   | 'xxs'
