74790681f5b5a626cf2632d4f93115bb425a6e08
dhoonjang <dhoonjang@gmail.com>
2021-02-04 13:54:33 +0900

    feat: textarea ref

--- src/component/Textarea/index.tsx
+++ src/component/Textarea/index.tsx
@@ -1,27 +1,30 @@
-import React, { TextareaHTMLAttributes } from 'react';
-import cn from 'classnames';
-import './index.scss';
+import React, { TextareaHTMLAttributes } from "react";
+import cn from "classnames";
+import "./index.scss";
 
 export interface ITextareaProps
   extends TextareaHTMLAttributes<HTMLTextAreaElement> {
-  borderType?: 'box' | 'single-line' | 'underline';
-  areaType?: 'scroll' | 'autosize'; // --> autosize는 따로 만들어야함 default textarea / autosize textarea
+  borderType?: "box" | "single-line" | "underline";
+  areaType?: "scroll" | "autosize"; // --> autosize는 따로 만들어야함 default textarea / autosize textarea
+  ref?: React.RefObject<HTMLTextAreaElement>;
 }
 export const Textarea: React.FC<ITextareaProps> = ({
   className,
   children,
-  borderType = 'box',
+  borderType = "box",
   disabled,
   spellCheck = false,
+  ref,
   ...props
 }) => (
   <textarea
-    className={cn('_TEXTAREA_', className, borderType, {
+    className={cn("_TEXTAREA_", className, borderType, {
       dis: disabled,
-      'dis-single-line': borderType === 'single-line' && disabled,
-      'dis-underline': borderType === 'underline' && disabled,
+      "dis-single-line": borderType === "single-line" && disabled,
+      "dis-underline": borderType === "underline" && disabled,
     })}
     {...props}
+    ref={ref}
   >
     {children}
   </textarea>
