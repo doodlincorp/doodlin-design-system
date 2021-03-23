ac51fcc6edd9d1acd43936c93629f02a88411180
chamdasu <68911837+chamdasu@users.noreply.github.com>
2021-03-23 14:09:46 +0900

    fix: Modal

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-import './reset.scss';
-export { default as Button } from './component/Button';
-export { default as Input } from './component/Input';
-export { default as Spinner } from './component/Spinner';
-export { default as Text } from './component/Text';
-export { default as Textarea } from './component/Textarea';
+import "./reset.scss";
+export { default as Button } from "./component/Button";
+export { default as Input } from "./component/Input";
+export { default as Spinner } from "./component/Spinner";
+export { default as Text } from "./component/Text";
+export { default as Textarea } from "./component/Textarea";
+export { default as Modal } from "./component/Modal";
 
 export type TDefaultSize =
-  | 'xxs'
-  | 'xs'
-  | 'sm'
-  | 'md'
-  | 'lg'
-  | 'xl'
-  | 'xxl'
-  | 'xxxl';
+  | "xxs"
+  | "xs"
+  | "sm"
+  | "md"
+  | "lg"
+  | "xl"
+  | "xxl"
+  | "xxxl";
