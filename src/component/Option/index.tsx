1d8a20a766ed15530d5617f26296e25ee6270c22
dhoonjang <dhoonjang@gmail.com>
2021-05-31 10:38:38 +0900

    fix'

--- src/component/Option/index.tsx
+++ src/component/Option/index.tsx
@@ -2,12 +2,20 @@ import React, { HTMLAttributes } from "react";
 import cn from "classnames";
 import "./index.scss";
 
-export interface IOptionProps {
+export interface IOptionProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
 }
 
-export const Option: React.FC<IOptionProps> = ({ className, children }) => {
-  return <div className={cn("_OPTION_", className)}>{children}</div>;
+export const Option: React.FC<IOptionProps> = ({
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <div className={cn("_OPTION_", className)} {...props}>
+      {children}
+    </div>
+  );
 };
 
 export default Option;
