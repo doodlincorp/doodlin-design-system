701676ff4abc1c3c1a1334d28d53ef6853300658
dhoonjang <dhoonjang@gmail.com>
2021-05-31 15:08:00 +0900

    fix

--- src/component/Option/index.tsx
+++ src/component/Option/index.tsx
@@ -1,19 +1,38 @@
-import React, { HTMLAttributes } from "react";
+import React, { HTMLAttributes, useRef } from "react";
 import cn from "classnames";
 import "./index.scss";
+import { useToggle } from "dhoonjang-s-hooks";
 
 export interface IOptionProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
+  placement?: "left" | "right";
+  btn: JSX.Element;
 }
 
 export const Option: React.FC<IOptionProps> = ({
   className,
   children,
+  btn,
+  placement = "left",
   ...props
 }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [optionOpen, setOptionOpen] = useToggle(ref);
+
   return (
-    <div className={cn("_OPTION_", className)} {...props}>
-      {children}
+    <div className={cn("_OPTION_", className)} {...props} ref={ref}>
+      <div
+        className="toggle"
+        onClick={(e) => {
+          setOptionOpen(false);
+          e.stopPropagation();
+        }}
+      >
+        {btn}
+      </div>
+      {optionOpen && (
+        <div className={cn("option-area", placement)}>{children}</div>
+      )}
     </div>
   );
 };
--- src/stories/Option.stories.tsx
+++ src/stories/Option.stories.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Button from "../component/Button";
 import { EditIcon } from "../component/Icon/EditIcon";
 import { GarbageIcon } from "../component/Icon/GarbageIcon";
 import { SettingIcon } from "../component/Icon/SettingIcon";
@@ -14,8 +15,8 @@ export default {
 export const Default = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div style={{ margin: 20, position: "relative" }}>
-      <Option>
+    <div style={{ margin: 20, position: "relative", padding: 20 }}>
+      <Option btn={<Button>option</Button>}>
         <OptionItem
           text="hello 소환"
           onClick={() => setOpen(!open)}
@@ -38,7 +39,29 @@ export const Default = () => {
       <br /> <br />
       <br /> <br />
       <br /> <br />
-      <br />
+      <Option btn={<Button>option</Button>} placement="right">
+        <OptionItem
+          text="hello 소환"
+          onClick={() => setOpen(!open)}
+        ></OptionItem>
+        <OptionItem text="아이콘과 함께" icon={<EditIcon />}></OptionItem>
+        <OptionItem text="로딩중" loading></OptionItem>
+        <OptionItem
+          text="비활성화"
+          icon={<SettingIcon />}
+          disabled
+        ></OptionItem>
+        <OptionItem
+          className="delete"
+          text="삭제"
+          icon={<GarbageIcon />}
+          variant="danger"
+        ></OptionItem>
+      </Option>
+      <br /> <br />
+      <br /> <br />
+      <br /> <br />
+      <br /> <br />
       {open && "/ hello! \\"}
     </div>
   );
