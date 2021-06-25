63eda25eb96af8b96a8306a7fa7f9d0f1fc3be60
chamdasu <68911837+chamdasu@users.noreply.github.com>
2021-06-25 18:12:28 +0900

    feat: Skeleton

---
+++ src/component/Skeleton/index.tsx
@@ -0,0 +1,9 @@
+import { SkeletonBox } from "./SkeletonBox";
+import { SkeletonBtn } from "./SkeletonBtn";
+
+const Skeleton = {
+  Box: SkeletonBox,
+  Btn: SkeletonBtn,
+};
+
+export default Skeleton;
