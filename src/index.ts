e35c851ed530cbcd98d7b17d9fdf8faea83b1492
chamdasu <68911837+chamdasu@users.noreply.github.com>
2021-07-07 10:12:38 +0900

    feat: shadow variant

--- src/index.ts
+++ src/index.ts
@@ -128,13 +128,23 @@ export const interview_orange_7 = `#F38119`;
 export const referral_blue_1 = `#E8EFF7`;
 export const referral_blue_7 = `#1864AB`;
 
-// Shadow
+// Shadow(legacy)
 export const shadow_sm = `0 2px 4px rgba(0, 0, 0, 0.1)`;
 export const shadow_md0 = `0 4px 8px rgba(0, 0, 0, 0.12)`;
 export const shadow_md1 = `0 2px 4px rgba(0, 0, 0, 0.08)`;
 export const shadow_lg0 = `0 5px 30px rgba(0, 0, 0, 0.12)`;
 export const shadow_lg1 = `0 5px 15px rgba(0, 0, 0, 0.08)`;
 export const shadow_option = `0 2px 6px rgba(0, 0, 0, 0.16)`;
+
+// Greeting Shadow
+export const shadow_1 = `0px 0px 1px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.24)`;
+export const shadow_2 = `0px 0px 2px rgba(0, 0, 0, 0.12), 0px 0px 16px rgba(0, 0, 0, 0.12)`;
+export const shadow_3 = `0px 3px 8px rgba(0, 0, 0, 0.15), 0px 1px 1px rgba(0, 0, 0, 0.06)`;
+export const shadow_4 = `0px 1px 1px rgba(0, 0, 0, 0.06), 0px 3px 12px rgba(0, 0, 0, 0.15)`;
+export const shadow_5 = `0px 4px 12px rgba(0, 0, 0, 0.16), 0px 1px 2px rgba(0, 0, 0, 0.08), 0px 1px 4px rgba(0, 0, 0, 0.08)`;
+export const shadow_6 = `0px 1px 2px rgba(0, 0, 0, 0.06), 0px 3px 12px rgba(0, 0, 0, 0.36)`;
+export const shadow_7 = `0px 1px 2px rgba(0, 0, 0, 0.2), 0px 8px 20px rgba(0, 0, 0, 0.24)`;
+export const shadow_inner = `inset 0px 2px 3px rgba(0, 0, 0, 0.12)`;
 
 // Container
 export const login_container_width = `772px`;
