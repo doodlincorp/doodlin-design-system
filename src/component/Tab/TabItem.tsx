e8cb0410834069ecd8c5c62b080f8dc43622183b
chamdasu <68911837+chamdasu@users.noreply.github.com>
2021-07-07 15:13:47 +0900

    fix: TabItem export issue

---
+++ src/component/Tab/TabItem.tsx
@@ -0,0 +1,38 @@
+import React, { HTMLAttributes } from "react";
+import cn from "classnames";
+import "./index.scss";
+
+export interface ITabItemProps extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  label?: string;
+  width?: number;
+  selected?: boolean;
+  icon?: { leading?: React.ReactElement; tailing?: React.ReactElement };
+  disabled?: boolean;
+}
+
+const TabItem: React.FC<ITabItemProps> = ({
+  className,
+  onClick,
+  label,
+  width,
+  selected,
+  icon,
+  disabled,
+  ...props
+}) => {
+  return (
+    <div
+      className={cn("_TABITEM_", { selected, iconOnly: !label, disabled })}
+      onClick={onClick}
+      style={{ width: width }}
+      {...props}
+    >
+      {icon?.leading && <div className="leading-icon">{icon.leading}</div>}
+      {label}
+      {icon?.tailing && <div className="tailing-icon">{icon.tailing}</div>}
+    </div>
+  );
+};
+
+export default TabItem;
