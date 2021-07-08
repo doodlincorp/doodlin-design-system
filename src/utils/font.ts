17a0dde560408b691d03945906387a860c7b24fd
chamdasu <68911837+chamdasu@users.noreply.github.com>
2021-07-08 14:52:44 +0900

    refac: enum fontWeight, fontSize, lineHeight

--- src/utils/font.ts
+++ src/utils/font.ts
@@ -1,23 +1,26 @@
 // Font Family
-export const font = `"Spoqa Han Sans Neo", "sans_serif"`;
+export const fontFamily = `"Spoqa Han Sans Neo", "sans_serif"`;
 
-// Font Weight
-export const font_weight_light = 300;
-export const font_weight_regular = 400;
-export const font_weight_medium = 500;
-export const font_weight_bold = 600;
+export enum EFontWeight {
+  light = 300,
+  regular = 400,
+  medium = 500,
+  bold = 600,
+}
 
-// Font Size
-export const font_size_xx_small = `1.2rem`;
-export const font_size_x_small = `1.3rem`;
-export const font_size_small = `1.4rem`;
-export const font_size_medium = `1.6rem`;
-export const font_size_large = `1.8rem`;
-export const font_size_x_large = `2.1rem`;
-export const font_size_xx_large = `2.4rem`;
-export const font_size_xxx_large = `3.6rem`;
-export const font_size_xxxx_large = `4.8rem`;
+export enum EFontSize {
+  xx_small = `1.2rem`,
+  x_small = `1.3rem`,
+  small = `1.4rem`,
+  medium = `1.6rem`,
+  large = `1.8rem`,
+  x_large = `2.1rem`,
+  xx_large = `2.4rem`,
+  xxx_large = `3.6rem`,
+  xxxx_large = `4.8rem`,
+}
 
-// Line Height
-export const line_height_medium = `1.5em`;
-export const line_height_large = `1.75em`;
+export enum ELineHeight {
+  medium = `1.5em`,
+  large = `1.75em`,
+}
