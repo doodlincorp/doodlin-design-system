071bf353284b9c37d55d79108adc02faefc98146
chamdasu <68911837+chamdasu@users.noreply.github.com>
2021-07-21 13:30:45 +0900

    fix: SelectBox Style

--- src/component/SelectBox/index.tsx
+++ src/component/SelectBox/index.tsx
@@ -3,6 +3,7 @@ import CustomSelect, { isObjectEqual } from "../CustomSelect";
 import { CheckIcon } from "../Icon/CheckIcon";
 import cn from "classnames";
 import "./index.scss";
+import { EColorMap } from "../../utils/colorMap";
 
 export interface ISelectBoxItem {
   icon?: JSX.Element | string;
@@ -45,18 +46,19 @@ const SelectBox: <T extends ISelectBoxItem>(
       )}
       getOptionViewFunc={(o) => (
         <div className={cn("option-view")} onClick={() => selectFunc(o)}>
-          {o.icon &&
-            (isObjectEqual(o, value) ? (
-              <div className="icon">
-                <CheckIcon />
-              </div>
-            ) : (
+          {isObjectEqual(o, value) ? (
+            <div className="icon">
+              <CheckIcon color={EColorMap.gray_6} />
+            </div>
+          ) : (
+            o.icon && (
               <div
                 className={cn("icon", { imoji: typeof o.icon === "string" })}
               >
                 {o.icon}
               </div>
-            ))}
+            )
+          )}
           <div className="txt">{o.text}</div>
         </div>
       )}
