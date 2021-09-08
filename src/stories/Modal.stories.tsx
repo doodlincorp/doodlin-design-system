e0e219c4abaab8320a6aaaadef8d1f67365ec51a
chansu park <ida9310311@gmail.com>
2021-09-08 21:05:29 +0900

    feat: Modal variant "alt" 추가

--- src/stories/Modal.stories.tsx
+++ src/stories/Modal.stories.tsx
@@ -17,6 +17,7 @@ export const Default = () => {
       <Button onClick={() => setOpen(true)}>모달 열기</Button>
       {open && (
         <Modal
+          variant="default"
           width={400}
           closeBtn={() => setOpen(false)}
           header={{ Text: "기본 모달" }}
@@ -34,6 +35,59 @@ export const Default = () => {
             BtnAlign: "end",
           }}
         >
+          <Input
+            icon={{ tailing: <SearchIcon /> }}
+            placeholder="다양한 키워드로 검색해 보세요"
+          />
+          <Input
+            icon={{ tailing: <SearchIcon /> }}
+            placeholder="다양한 키워드로 검색해 보세요"
+          />
+          <Input
+            icon={{ tailing: <SearchIcon /> }}
+            placeholder="다양한 키워드로 검색해 보세요"
+          />
+          <Input
+            icon={{ tailing: <SearchIcon /> }}
+            placeholder="다양한 키워드로 검색해 보세요"
+          />
+          <Input
+            icon={{ tailing: <SearchIcon /> }}
+            placeholder="다양한 키워드로 검색해 보세요"
+          />
+          <Input
+            icon={{ tailing: <SearchIcon /> }}
+            placeholder="다양한 키워드로 검색해 보세요"
+          />
+          <Input
+            icon={{ tailing: <SearchIcon /> }}
+            placeholder="다양한 키워드로 검색해 보세요"
+          />
+          <Input
+            icon={{ tailing: <SearchIcon /> }}
+            placeholder="다양한 키워드로 검색해 보세요"
+          />
+          <Input
+            icon={{ tailing: <SearchIcon /> }}
+            placeholder="다양한 키워드로 검색해 보세요"
+          />
+          <Input
+            icon={{ tailing: <SearchIcon /> }}
+            placeholder="다양한 키워드로 검색해 보세요"
+          />
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+export const Variant = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div style={{ padding: 20 }}>
+      <Button onClick={() => setOpen(true)}>모달 열기</Button>
+      {open && (
+        <Modal width={400} variant="alt" closeBtn={() => setOpen(false)}>
           <Input
             icon={{ tailing: <SearchIcon /> }}
             placeholder="다양한 키워드로 검색해 보세요"
