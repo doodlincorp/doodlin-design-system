c7265f6045643e3201fcf8fd623e7756dfcd2c85
chansu park <ida9310311@gmail.com>
2022-02-15 12:10:16 +0900

    fix: minusIcon 정렬

--- src/component/Icon/MinusIcon.tsx
+++ src/component/Icon/MinusIcon.tsx
@@ -34,7 +34,7 @@ export const MinusIcon: React.FC<IMinusIconProps> = ({
         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect
             id="icon__fill"
-            x="4"
+            x="5"
             y="11"
             width="14"
             height="2"
